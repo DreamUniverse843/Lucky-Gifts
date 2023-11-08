d55217718e9e5d974dc45f63e27df2c2a9ea7199
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2023-11-08 15:45:11 +0800

    primarily support randomize assigning

--- gifts.py
+++ gifts.py
@@ -1,29 +1,43 @@
 # 本程序中涉及数组的正常使用 0 下标位，输出时请下标 + 1，下同。
 # 开数组时请正常开，输出时加一。
+import random
 
 import numpy
 
 num = int(input("请输入参与人数："))
+
 student = [i + 1 for i in range(num)]  # student——记学生序列
-sumPerson = num  # sumPerson——记总人数
+gift = [i + 1 for i in range(num)]  # gift——记礼物序列
+selfGift = 0  # 抽签时统计是否存在自己分配给自己的事件数
 
-isMultiGift = input("是否存在一个学生多个礼物的情况？(y/N)")
-if isMultiGift == "y":
-    multiGiftLen = int(input("输入多个礼物的学生人数:"))
-    multiGiftList = numpy.zeros((multiGiftLen, 2))  # 初始化值为 0 的二维数组——记多礼物学生人数和礼物个数
-    print("接下来请输入学生号和礼物数，以逗号分隔。\n例如，1 号同学有 3 个礼物，请输入 1,3 。\n")
-    for person in range(multiGiftLen):
-        src = input("输入第 " + str(person + 1) + " 位同学的编号和礼物数：")
-        stu = src.split(",", 1)
-        print(stu)
-        multiGiftList[person][0] = stu[0]
-        multiGiftList[person][1] = stu[1]
-    print(multiGiftList)
-    print("存在多个礼物的同学如下:\n" + numpy.lexsort(multiGiftList[0, None]) + "")
+print(student)
+print(gift)
+
+randNum = input("请输入打乱次数(如不输则在 1-10000 随机取数)：")
+
+if randNum == "":
+    randNum = random.randint(1, 10000)
+    for i in range(int(randNum)):
+        numpy.random.shuffle(gift)
+    for i in range(int(randNum)):
+        numpy.random.shuffle(student)
 else:
-    print(student)
-    numpy.random.shuffle(student)
-    print("随机打乱后的交换对象:")
-    print(student)
+    for i in range(int(randNum)):
+        numpy.random.shuffle(gift)
+    for i in range(int(randNum)):
+        numpy.random.shuffle(student)
 
-    # print(gift)
+# print("随机打乱后的交换对象:")
+# print(student)
+# print(gift)
+
+for person in range(num):
+
+    if str(student[person]) == str(gift[person]):
+        selfGift = selfGift + 1
+        print("在数组下标第 " + str(person) + " 处发生意外的自主分配: 学生 " + str(student[person]) + " 取走了 " + str(gift[person]) + " 的礼物。")
+print("发生自己分配给自己的事件数: " + str(selfGift))
+
+print("以下是交换礼品结果~")
+for person in range(num):
+    print(str(person) + ": 请 " + str(student[person]) + " 号同学领取 " + str(gift[person]) + " 号礼品~")
